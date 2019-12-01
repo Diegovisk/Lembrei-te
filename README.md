259934a64924c64c16042931be234971a5caa34e
Diego Lucena <diego_lucena01@live.com>
2019-11-30 22:12:28 -0400

    Fix archives

---
+++ README.md
@@ -0,0 +1,2 @@
+# Lembrei-te
+Projeto para a disciplina de Modelagem e Projetos de Sistemas da UEA
---
+++ src/js/view/lembretes.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // var msnry = new Masonry( '.lembretes-holder .columns', {
+    //     itemSelector: '.lembrete.box',
+    //     gutter:0,
+    //     columnWidth:'.grid-sizer',
+    //     percentPosition: true
+    // });
+});
